8bba921dd39486da4edeb68dbc26b84d3d2c426a
Sahand Ghavidel <sahand.gha@gmail.com>
2024-08-01 17:07:09 +0700

    Update dbName

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -12,7 +12,7 @@ export const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'next auth app',
+      dbName: 'next-auth-app',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
